cd4b2b206ea8f9d78c62f39ad8cfbd93a09f1c29
Thadeu <marciothadeu1984@gmail.com>
2026-06-23 21:09:50 -0300

    fix(security_groups): align with reference sg.tf
    - target cluster SG instead of node SG (covers Fargate pods)
    - nodeport range: 32767 → 32768
    - coredns: self-referencing → 0.0.0.0/0
    - add ports 80, 443, 8080, 8443

--- security_groups.tf
+++ security_groups.tf
@@ -1,22 +1,12 @@
-data "aws_security_group" "node" {
-  tags = {
-    "aws:eks:cluster-name" = aws_eks_cluster.this.name
-  }
-
-  depends_on = [
-    aws_eks_node_group.main,
-    aws_eks_node_group.spot,
-    aws_eks_node_group.critical,
-    aws_eks_node_group.graviton,
-    aws_eks_node_group.graviton_spot,
-  ]
+locals {
+  cluster_sg_id = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "nodeport" {
-  security_group_id = data.aws_security_group.node.id
-  description       = "Allow NodePort services from VPC"
+  security_group_id = local.cluster_sg_id
+  description       = "Allow NodePort services"
   from_port         = 30000
-  to_port           = 32767
+  to_port           = 32768
   ip_protocol       = "tcp"
   cidr_ipv4         = "0.0.0.0/0"
 
@@ -24,23 +14,67 @@ resource "aws_vpc_security_group_ingress_rule" "nodeport" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "coredns_tcp" {
-  security_group_id            = data.aws_security_group.node.id
-  description                  = "Allow CoreDNS TCP between nodes"
-  from_port                    = 53
-  to_port                      = 53
-  ip_protocol                  = "tcp"
-  referenced_security_group_id = data.aws_security_group.node.id
+  security_group_id = local.cluster_sg_id
+  description       = "Allow CoreDNS TCP"
+  from_port         = 53
+  to_port           = 53
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
 
   tags = merge(var.tags, { Name = "${var.cluster_name}-coredns-tcp" })
 }
 
 resource "aws_vpc_security_group_ingress_rule" "coredns_udp" {
-  security_group_id            = data.aws_security_group.node.id
-  description                  = "Allow CoreDNS UDP between nodes"
-  from_port                    = 53
-  to_port                      = 53
-  ip_protocol                  = "udp"
-  referenced_security_group_id = data.aws_security_group.node.id
+  security_group_id = local.cluster_sg_id
+  description       = "Allow CoreDNS UDP"
+  from_port         = 53
+  to_port           = 53
+  ip_protocol       = "udp"
+  cidr_ipv4         = "0.0.0.0/0"
 
   tags = merge(var.tags, { Name = "${var.cluster_name}-coredns-udp" })
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = local.cluster_sg_id
+  description       = "Allow HTTP"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+
+  tags = merge(var.tags, { Name = "${var.cluster_name}-http" })
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = local.cluster_sg_id
+  description       = "Allow HTTPS"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+
+  tags = merge(var.tags, { Name = "${var.cluster_name}-https" })
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http_alt" {
+  security_group_id = local.cluster_sg_id
+  description       = "Allow HTTP alternate"
+  from_port         = 8080
+  to_port           = 8080
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+
+  tags = merge(var.tags, { Name = "${var.cluster_name}-http-alt" })
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https_alt" {
+  security_group_id = local.cluster_sg_id
+  description       = "Allow HTTPS alternate"
+  from_port         = 8443
+  to_port           = 8443
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+
+  tags = merge(var.tags, { Name = "${var.cluster_name}-https-alt" })
 }
